da3cc52d0cf5f139f3ca20083b1c41114c5265e1
AurelTBE <aureltrouble@gmail.com>
2019-12-13 23:55:24 +0100

    Rollback notif serv

--- public/static/fcm.js
+++ public/static/fcm.js
@@ -2,14 +2,14 @@ importScripts("https://www.gstatic.com/firebasejs/7.6.0/firebase-app.js");
 importScripts("https://www.gstatic.com/firebasejs/7.6.0/firebase-messaging.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyBUwfqjKr5-kzmpjcXX4qOOiMilgeCsLjM",
-  authDomain: "notificationssga.firebaseapp.com",
-  databaseURL: "https://notificationssga.firebaseio.com",
-  projectId: "notificationssga",
-  storageBucket: "notificationssga.appspot.com",
-  messagingSenderId: "1012938001250",
-  appId: "1:1012938001250:web:44393feeec0ad5e9653a3b",
-  measurementId: "G-FBMGGHCFPB"
+  apiKey: "AIzaSyDIFMxGHgJ64un5OQmiUQW-1umTaEpY8TA",
+  authDomain: "notifications-sga.firebaseapp.com",
+  databaseURL: "https://notifications-sga.firebaseio.com",
+  projectId: "notifications-sga",
+  storageBucket: "notifications-sga.appspot.com",
+  messagingSenderId: "539284504552",
+  appId: "1:539284504552:web:9eb3d24371c3ea28352e5d",
+  measurementId: "G-ND9S7BEXVX"
 });
 
 firebase.messaging()
--- utils/webPush.js
+++ utils/webPush.js
@@ -6,14 +6,14 @@ import firebase from 'firebase/app'
 const firebaseCloudMessaging = {
   init: async function () {
     firebase.initializeApp({
-      apiKey: "AIzaSyBUwfqjKr5-kzmpjcXX4qOOiMilgeCsLjM",
-      authDomain: "notificationssga.firebaseapp.com",
-      databaseURL: "https://notificationssga.firebaseio.com",
-      projectId: "notificationssga",
-      storageBucket: "notificationssga.appspot.com",
-      messagingSenderId: "1012938001250",
-      appId: "1:1012938001250:web:44393feeec0ad5e9653a3b",
-      measurementId: "G-FBMGGHCFPB"
+      apiKey: "AIzaSyDIFMxGHgJ64un5OQmiUQW-1umTaEpY8TA",
+      authDomain: "notifications-sga.firebaseapp.com",
+      databaseURL: "https://notifications-sga.firebaseio.com",
+      projectId: "notifications-sga",
+      storageBucket: "notifications-sga.appspot.com",
+      messagingSenderId: "539284504552",
+      appId: "1:539284504552:web:9eb3d24371c3ea28352e5d",
+      measurementId: "G-ND9S7BEXVX"
     })
 
     try {
