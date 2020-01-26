494f182d2850c8663c7e054dcba01002fb0b1506
AurelTBE <aureltrouble@gmail.com>
2020-01-26 22:17:29 +0100

    Error page v1.1

--- pages/_error.js
+++ pages/_error.js
@@ -1,5 +1,6 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 
@@ -21,29 +22,31 @@ function Error({ statusCode }) {
 
   return (
     <Layout>
-      <Grid
-        container
-        spacing={0}
-        direction="column"
-        alignItems="center"
-        justify="center"
-        style={{ minHeight: '85vh' }}
-      >
-        <Grid item>
-          <FontAwesomeIcon icon={faDragon} style={{ fontSize: 40, marginBottom: 20 }} className={classes.icon} />
+      <Box p={3}>
+        <Grid
+          container
+          spacing={0}
+          direction="column"
+          alignItems="center"
+          justify="center"
+          style={{ minHeight: '85vh' }}
+        >
+          <Grid item>
+            <FontAwesomeIcon icon={faDragon} style={{ fontSize: 40, marginBottom: 20 }} className={classes.icon} />
+          </Grid>
+          <Grid item>
+            {statusCode
+              ? 
+              <Typography variant="h5" color="Primary" gutterBottom>Une erreur {statusCode} s'est produite sur le serveur. La page n'a pas été trouvée...</Typography>
+              : 
+              <Typography variant="h5" color="Primary" gutterBottom>Une erreur s'est produite sur votre appareil, vérifiez votre connexion ou réessayez plus tard.</Typography>
+            }
+          </Grid>
+          <Button variant="contained" color="primary" href="/">
+            Retourner à l'Accueil
+          </Button>
         </Grid>
-        <Grid item>
-          {statusCode
-            ? 
-            <Typography variant="h5" color="Primary" gutterBottom>Une erreur {statusCode} s'est produite sur le serveur. La page n'a pas été trouvée...</Typography>
-            : 
-            <Typography variant="h5" color="Primary" gutterBottom>Une erreur s'est produite sur votre appareil, vérifiez votre connexion ou réessayez plus tard.</Typography>
-          }
-        </Grid>
-        <Button variant="contained" color="primary" href="/">
-          Retourner à l'Accueil
-        </Button>
-      </Grid>
+      </Box>
     </Layout>
   )
 }
